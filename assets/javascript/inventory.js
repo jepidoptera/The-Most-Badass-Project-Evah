4b1cace802422ae7cff6cc9629553ffc7e10b4ee
Smarty Pants <amcurrent@hotmail.com>
2019-03-19 10:45:02 -0500

    added buttons, re-arranged layout

--- assets/javascript/inventory.js
+++ assets/javascript/inventory.js
@@ -1,4 +1,27 @@
+
 // jshint esversion: 6
+// var inventory = [];
+$(document).ready(function(){
 
+    var config = {
+        apiKey: "AIzaSyAqd2f2a2iFdzSMNqsGzt8ShzjJ_VxSG9w",
+        authDomain: "pokemon-trail.firebaseapp.com",
+        databaseURL: "https://pokemon-trail.firebaseio.com",
+        projectId: "pokemon-trail",
+        storageBucket: "",
+        messagingSenderId: "726250890927"
+    };
+    firebase.initializeApp(config);
 
-alert('hello');
+    var database = firebase.database();
+
+    var pokeBalls = 0;
+    var money = 0;
+    var kibble = 0;
+    var food = 0;
+
+    // $('.main-menu').hide();
+
+    $('#submit').on('click', function(e){
+        e.preventDefault();
+   
