72eebcdc205c0345e409ecba1d0622446da21086
Smarty Pants <amcurrent@hotmail.com>
2019-03-21 17:02:11 -0500

    saving so I can look at conflicts

--- assets/javascript/shop.js
+++ assets/javascript/shop.js
@@ -1,4 +1,33 @@
 
+// function transaction(shopper,store,item,count){
+//     var cost = store.inventory[item].cost,
+//         vol = store.inventory[item].count;
+    
+//     if (shopper.money < (cost * count)) {
+//       alert("Not enough money.");
+//       return;
+//     }
+//     if (store.inventory[item].count < count){
+//       alert("We don't have that many " + item);
+//       return;
+//     }
+    
+//     shopper.money -= (cost * vol);
+//     // seller.money += (c * vol);
+//     shopper.inventory[item].count += count;   
+//     store.inventory[item].count -= count;
+    
+//   }
+
+var store = {
+    inventory: {
+      balls:{count:50,cost:5},
+      food:{count:50,cost:10},
+      kibble:{count:50,cost:10}
+    },
+
+    
+}
 
 
 function onBuyAPoke() {
@@ -20,13 +49,6 @@ function onBuyKibble(){
 
 
 
-// var store = {
-//     inventory: {
-//       balls:{count:50,cost:5},
-//       food:{count:50,cost:10},
-//       kibble:{count:50,cost:10}
-//     }
-//   }
 
 function firebaseReady(){
   var shopper = {
@@ -38,16 +60,18 @@ function firebaseReady(){
 
 onBuyAPoke();
 $('#pBal').append(shopper.balls);
-// $('#sBal').append(store.inventory.balls);
+$('#sBal').append(store.inventory.balls.count);
 
    onBuyKibble();
 $('#pKib').append(shopper.kibble);
-   
+$('#sKib').append(store.inventory.kibble.count);
  onBuyFood();
  $('#pFoo').append(shopper.food);
+ $('#sFoo').append(store.inventory.food.count);
 
  $('#pMon').append(shopper.money);
-
+ 
+ transaction();
  };
 
 
