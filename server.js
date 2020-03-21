681a83c74a2b755744e44d20b071908852967a52
jepidoptera <jepidoptera@live.com>
2020-03-21 15:10:23 -0500

    added a server

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const trail = require('trail');
+
+const port = process.env.PORT || 8080;
+const apiKey = process.env.APIKEY || "no_api_key";
+
+// Serve static files
+app.use(express.static(__dirname + '/public'));
+
+// use handlebars
+var exphbs = require("express-handlebars");
+app.engine(".hbs", exphbs({
+    defaultLayout: "main",
+    extname: ".hbs",
+    helpers: {
+    // capitalize words on request
+        'Capitalize': function(string)
+        {
+            return string.charAt(0).toUpperCase() + string.slice(1);
+        }
+    }
+}));
+
+app.set("view engine", ".hbs");
+
+// Serve app
+console.log('Listening on: http://localhost:' + port);
+
+app.get ("/", (req, res) => {
+    res.render("index");
+})
+
+app.listen(port);
