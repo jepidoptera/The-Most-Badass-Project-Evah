76a9ec3e50371cf4bb0cd66d36d8750c2f84c07d
jepidoptera <jepidoptera@live.com>
2020-03-21 15:15:27 -0500

    bare bones

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const trail = require('trail');
+// const trail = require('trail');
 
 const port = process.env.PORT || 8080;
-const apiKey = process.env.APIKEY || "no_api_key";
+// const apiKey = process.env.APIKEY || "no_api_key";
 
 // Serve static files
 app.use(express.static(__dirname + '/public'));
 
 // use handlebars
-var exphbs = require("express-handlebars");
-app.engine(".hbs", exphbs({
-    defaultLayout: "main",
-    extname: ".hbs",
-    helpers: {
-    // capitalize words on request
-        'Capitalize': function(string)
-        {
-            return string.charAt(0).toUpperCase() + string.slice(1);
-        }
-    }
-}));
+// var exphbs = require("express-handlebars");
+// app.engine(".hbs", exphbs({
+//     defaultLayout: "main",
+//     extname: ".hbs",
+//     helpers: {
+//     // capitalize words on request
+//         'Capitalize': function(string)
+//         {
+//             return string.charAt(0).toUpperCase() + string.slice(1);
+//         }
+//     }
+// }));
 
-app.set("view engine", ".hbs");
+// app.set("view engine", ".hbs");
 
 // Serve app
 console.log('Listening on: http://localhost:' + port);
 
 app.get ("/", (req, res) => {
-    res.render("index");
+    res.sendFile("index");
 })
 
 app.listen(port);
