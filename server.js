a7dbde561dc5485ac32eaf877c069bfe43fc1e71
jepidoptera <jepidoptera@live.com>
2020-03-26 21:20:20 -0500

    continuous synching, better auth

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ firebase.initializeApp({
 const port = process.env.PORT || 8080;
 // const apiKey = process.env.APIKEY || "no_api_key";
 const authtokens = {};
+const players = [];
 
 // Serve static files
 app.use(express.static(__dirname + '/public'));
@@ -45,98 +46,100 @@ app.get('/', (req, res) => {
     res.render('login', {});
 })
 
-app.get("/login", (req, res) => {
-    console.log("logging in as...", req.query.username);
-    let username = req.query.username;
-    let password = req.query.password;
-    let playerRef = firebase.database().ref('users/' + username + "/gameInfo");
+app.post("/login", (req, res) => {
+    console.log("logging in as...", req.body.username);
+    let username = req.body.username;
+    let password = req.body.password;
     console.log(req.body);
-    res.query = null;
-    playerRef.on('value', value => {
-        if (value.val() && value.val().password) {
-            if (password != value.val().password) {
-                res.sendStatus(403);
-                return;
-            }
-        }
-        authtokens[username] = generateToken();
-        console.log(authtokens[username])
-        res.redirect(`journey?name=${username}&auth=${authtokens[username]}`);
-        res.end();
-    })
+    if (!players[username]) {
+        res.status(404).send('Player not found.')
+        return;
+    }
+    else if ((players[username].password || password) && password != players[username].password) {
+        res.status(403).send('Wrong password.');
+        return;
+    }
+    authtokens[username] = generateToken();
+    console.log(authtokens[username])
+    res.json({authtoken: authtokens[username]});
+    res.end();
 })
 
 app.get ("/journey", (req, res) => {
     console.log("let's go")
-    let playerID = req.query.name;
+    let username = req.query.name;
     let authtoken = req.query.auth;
-    if (!authtoken || authtokens[playerID] != authtoken) {
-        res.sendStatus(403);
+    if (!authtoken || authtokens[username] != authtoken) {
+        console.log("got auth token " + authtoken + " vs expected " + authtokens[username]);
+        res.render('logout');
         return;
     }
-    let playerRef = firebase.database().ref('users/' + playerID + "/gameInfo");
-    res.query = null;
-    playerRef.on('value', value => {
-        if (!value.val()) {
-            res.render("journey", {name: playerID})
-            return;
-        }
-        res.render("journey", {...value.val(), authtoken: authtoken});
-    })
+    if (!players[username]) {
+        res.render("journey", {name: username})
+        return;
+    }
+    res.render("journey", {...players[username], authtoken: authtoken});
+
 })
 
 app.get ("/hunt", (req, res) => {
     console.log("going hunting")
-    let playerID = req.query.name;
+    let username = req.query.name;
     let authtoken = req.query.auth;
-    console.log("got auth token " + authtoken + " vs expected " + authtokens[playerID]);
-    if (!authtoken || authtokens[playerID] != authtoken) {
-        res.sendStatus(403);
+    if (!authtoken || authtokens[username] != authtoken) {
+        console.log("got auth token " + authtoken + " vs expected " + authtokens[username]);
+        res.render('logout');
         return;
     }
-    let playerRef = firebase.database().ref('users/' + playerID + "/gameInfo");
-    playerRef.on('value', value => {
-        if (value.val().password) {
-            if (password != value.val().password) {
-                res.sendStatus(403);
-            }
-        }
-        res.render("hunt", value.val());
-    })
+    res.render("hunt", {...players[username], authtoken: authtoken});
 })
 
 app.post("/save", (req, res) => {
-    console.log('saving...');
-    req.body = JSON.parse(req.body.player)
-    console.log(req.body);
-    let playerID = req.body.name;
-    let playerRef = firebase.database().ref('users/' + playerID + "/gameInfo");
-    playerRef.off('value');
-    Object.keys(req.body).forEach(key => {
-        firebase.database().ref('users/' + playerID + "/gameInfo/" + key).set(req.body[key])
-    })
+    req.body = JSON.parse(req.body.data);
+    console.log("***saving***", req.body);
+    let username = req.body.name;
+    let authtoken = req.body.authtoken;
+    if (authtoken !== authtokens[username]) {
+        console.log("got auth token " + authtoken + " vs expected " + authtokens[username]);
+        res.render('logout');
+        return;
+    }
+    players[username] = {...players[username], ...req.body}
+    firebase.database().ref(`users/${username}`).set(players[username])
+    res.status(200).send("saved.");
+})
+ 
+app.post("/new", (req, res) => {
+    console.log("creating new player...", req.body.name);
+    let username = req.body.username;
+    let password = req.body.password;
+    if (players[username]) {
+        res.status(403).send('Username already exists.')
+    }
+    else {
+        firebase.database().ref('users/' + username).set({name: username, password: password, progress: 0})
+        authtokens[username] = generateToken();
+        res.json({authtoken: authtokens[username]});
+    }
     res.end();
 })
 
-app.get('/load/:playerID/:authtoken', (req, res) => {
-    console.log('loading', req.params['playerID'])
-    let playerID = req.params['playerID'];
+app.get('/load/:username/:authtoken', (req, res) => {
+    console.log('loading', req.params['username'])
+    let username = req.params['username'];
     let authtoken = req.params['authtoken'];
-    if (authtoken != authtokens[playerID]) {
-        console.log("got auth token " + authtoken + " vs expected " + authtokens[playerID]);
+    if (authtoken != authtokens[username]) {
+        console.log("got auth token " + authtoken + " vs expected " + authtokens[username]);
         res.sendStatus(403);
         return;
     }
-    let playerRef = firebase.database().ref('users/' + playerID + "/gameInfo");
-    // listen for player value (download entire player object every time anything changes)
-    playerRef.on('value', (value) => {
-        if (!value.val()) {
-            console.log('not found.');
-            res.sendStatus(404);
-            return;
-        }
-        res.json({...value.val(), authtoken: authtokens[playerID]});
-    });
+    if (!players[username]) {
+        console.log('not found.');
+        res.sendStatus(404);
+        return;
+    }
+    res.json({...players[username], authtoken: authtokens[username]});
+
 })
 app.listen(port);
 
@@ -145,3 +148,14 @@ function generateToken() {
     // string some random numbers and letters together
     return [1, 2, 3, 4, 5, 6, 7].map(n => chars[Math.floor(Math.random() * chars.length)]).join('');
 }
+
+// keep up to date with firebase
+firebase.database().ref('users').on('child_added', (child) => {
+    player = child.val();
+    let playerName = player.name;
+    players[playerName] = player;
+    firebase.database().ref('users/' + playerName).on('value', value => {
+        players[playerName] = value.val();
+        console.log("loaded player: ", playerName);
+    })
+})
